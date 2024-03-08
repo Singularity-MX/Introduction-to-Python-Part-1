5cdd5cf33bed598a82e8402551d1ad25441fe6d6
Cesar D <112652221+cessdel@users.noreply.github.com>
2024-03-08 08:58:09 -0600

    Ejercicio de condicionales 2

---
+++ EjemplosResueltos/Ejercicio5.py
@@ -0,0 +1,13 @@
+# Creamos la variable y le asignamos un valor
+numero = 150
+
+# Usamos la sentencia if para evaluar el valor de la variable
+if numero < 100:
+  # Si el valor es menor que 100, se imprime "hola"
+  print("hola")
+elif numero >= 100 and numero <= 200:
+  # Si el valor está entre 100 y 200, se imprime "chao"
+  print("chao")
+else:
+  # Si el valor no cumple ninguna de las condiciones anteriores, se imprime "adiós"
+  print("adiós")
