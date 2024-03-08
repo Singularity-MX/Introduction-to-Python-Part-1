bc967451995a8184aed6c8b5d2775e18398ce7c7
Cesar D <112652221+cessdel@users.noreply.github.com>
2024-03-08 08:55:40 -0600

    Create Ejercicio4.py
    Ejercicio 1 de condicionales

---
+++ EjemplosResueltos/Ejercicio4.py
@@ -0,0 +1,12 @@
+valor = 1000
+if valor > 100:
+  print(’Tengo una moto’)
+else:
+  print(’Tengo un coche’)
+## ¿Y al ejecutar este otro?
+valor1 = 30
+valor2 = 80
+if valor1 + valor2 < 100:
+  print(’Tengo una moto’)
+else:
+  print(’Tengo un coche’)
